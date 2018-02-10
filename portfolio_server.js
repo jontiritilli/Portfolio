4fd63a54fca42bdc205a0715458cf25aa5c272ed
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-02-09 17:26:34 -0800

    updated server

--- portfolio_server.js
+++ portfolio_server.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const app = express();
 
+const match = './memory_match';
+
 app.use(express.static(path.join(__dirname,'rootdir')))
 app.get('/memory_match', function(req,res){
-    
+    res.send(match)
 });
 
 app.listen(3000, function(){
