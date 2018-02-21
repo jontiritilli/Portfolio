e37fc36bbb3d30de485e19924666cc62f3a5ea10
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-02-21 11:00:35 -0800

    amended server port, rearranged contact info

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.port || 9000;
+const PORT = process.env.port || 8000;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client')))
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, '..', 'client', 'index.html'));
 })
 
-app.get('/memory_match', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'client', 'memory_match', 'index.html'));
+app.get('/memory_match', (req, res) => {
+    res.sendFile(resolve(__dirname, '..', 'client', 'memory_match', 'index.html'));
 })
 
-app.get('/calculator', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'client', 'calculator', 'index.html'));
+app.get('/calculator', (req, res) => {
+    res.sendFile(resolve(__dirname, '..', 'client', 'calculator', 'index.html'));
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, () => {
     console.log('The system is down on ', PORT)
 })
