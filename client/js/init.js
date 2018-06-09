f2ac8b9e2ca0c1a64bea05e27f29072794582c5d
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-06-09 10:51:45 -0700

    adjusted all colors, blur to background, rewrote sections

--- client/js/init.js
+++ client/js/init.js
@@ -17,6 +17,7 @@
     //deploy modals
     $('.modal').modal();
     $('.navBtn').on('click', ()=>{
+      console.log(event.target)
       scrollAndHide(event.target.id);
     });
     $('.sendBtn').on('click', () => {
@@ -30,7 +31,38 @@
     $.validator.addMethod("lettersOnly", function (value, element) {
       return this.optional(element) || /^([a-zA-Z0-9]+\s?[a-zA-Z0-9])*$/.test(value)
     }, "Alpha only");
+    $('img.svg').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
 
+      jQuery.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+
+          // Check if the viewport is set, else we gonna set it if we can.
+          if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+              $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+          }
+
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+
+      }, 'xml');
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
   function addTopBtn(){
