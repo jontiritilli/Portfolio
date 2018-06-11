80ec38ff439220ac1d13c2570210c24661ce1b4e
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-06-11 09:54:04 -0700

    added meta tags for opengraph, and set up server to server custom og image

--- server/master_server.js
+++ server/master_server.js
@@ -16,7 +16,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname, '..', 'client')))
 
+app.use(express.static(path.join(__dirname, '..','client','assets','images','backgrounds')))
+
 require('./mailer/node_mailer')(app)
+
+app.get('/ogimage', (req,res)=>{
+  res.send('linkImage.png');
+})
 
 app.listen(PORT, () => {
     console.log('The system is down on ', PORT)
