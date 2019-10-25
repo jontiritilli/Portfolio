bdae250f033ccafd09d16164d7f3a49d568c6484
Jon Tiritilli <jonathantiritilli@gmail.com>
2019-10-25 09:59:33 -0700

    add index and html markup for new design

--- client/js/init.js
+++ client/js/init.js
@@ -1,14 +1,10 @@
 (function($){
   $(function () {
-    window.addEventListener('scroll', () => {
+    document.body.addEventListener('scroll', () => {
       addTopBtn();
     })
-    $('#topBtn').on('click', () => {
+    $('#top-btn').on('click', () => {
       topFunction();
-    })
-    $('.mobile-close-nav').on('click', () => {
-      event.preventDefault();
-      closeSideNav();
     })
     $('.button-collapse').sideNav({
       draggable: true
@@ -16,15 +12,10 @@
     $('.parallax').parallax();
     //deploy modals
     $('.modal').modal();
-    $('.navBtn').on('click', (event)=>{
-      scrollAndHide($(event.target).attr('scrolltarget'));
+    $('a').on('click', (event)=>{
+      let scrolltarget = $(event.target).parent().attr("href");
+      scrollAndHide(scrolltarget);
     });
-    $('.landBtn').removeClass('scale-out');
-    $('.sendBtn').on('click', () => {
-      sendTrying();
-      sendMail();
-    });
-    validate();
     $.validator.methods.email = function (value, element) {
       return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/.test(value);
     }
@@ -67,10 +58,13 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
   function addTopBtn(){
-    if (window.scrollY > 65) {
-      return $('#topBtn').removeClass('scale-out');
+    let showPosition = 82;
+    let scrollPos = $(document.body).scrollTop();
+    if (scrollPos > showPosition) {
+      $('#top-btn').removeClass('scale-out');
+    } else {
+      $('#top-btn').addClass('scale-out');
     }
-      $('#topBtn').addClass('scale-out');
   }
   function topFunction() {
     $('html, body').animate({
@@ -78,7 +72,7 @@
     }, 750);
   }
   function scrollAndHide(target){
-    closeSideNav();
+    console.log("scroll and hide", "target", target);
     if(target){
       $('html, body').animate({
         scrollTop: $(target).offset().top
@@ -88,95 +82,8 @@
   function closeSideNav(){
     $('.button-collapse').sideNav('hide');
   }
-  function sendMail(){
-    if (!$(".contactForm").valid()){
-      sendFailed();
-      return
-    }
-    $.ajax({
-      url: '/send',
-      data: $('.contactForm').serialize(),
-      method: 'post',
-      success: function(res){
-        $('input, textarea').prop({
-          disabled: true
-        });
-        sendComplete();
-      },
-      error: function(err){
-        $('.modalHeader').text('Error');
-        $('.modalMessage').text('There was an issue sending your message, please try again');
-        $('.modal').modal('open')
-        sendFailed();
-      }
-    })
-  }
   function appendContactInfo($) {
     $('#text-number').attr('href', 'sms:951-282-7630');
     $('#call-number').attr('href', 'tel:951-282-7630');
     $('#contact-email').attr('href', 'mailto:JonTiritilli@gmail.com');
-  }
-  function validate (){
-    $(".contactForm").validate({
-      rules: {
-        name: {
-          required: true,
-          lettersOnly: true,
-          minlength: 2
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        message: {
-          required: true,
-          minlength: 15
-        },
-      },
-      //For custom messages
-      messages: {
-        name: {
-          required: "Please provide your name",
-          lettersOnly: 'Use only text characters',
-          minlength: "Enter at least 2 characters"
-        },
-        message: {
-          required: "Please enter a message",
-          minlength: "Enter at least 15 characters"
-        }
-      },
-      errorElement: 'div',
-      errorPlacement: function (error, element) {
-        var placement = $(element).data('error');
-        if (placement) {
-          $(placement).append(error)
-        } else {
-          error.insertAfter(element);
-        }
-      }
-    });
-  }
-
-  function sendTrying(){
-    let btn = $('.sendBtn');
-    btn.addClass('sending');
-    btn.empty();
-    let spinner = $('<div>');
-    spinner.append('Sending<i class="fas fa-spinner fa-pulse"></i>');
-    btn.append(spinner);
-  }
-
-  function sendFailed(){
-    let btn = $('.sendBtn')
-    btn.removeClass('sending');
-    btn.empty();
-    btn.append('Send<i class="material-icons right">send</i>');
-  }
-
-  function sendComplete(){
-    let btn = $('.sendBtn')
-    btn.addClass('disabled')
-    btn.removeClass('sending');
-    btn.empty();
-    btn.append('Sent<i class="material-icons right">send</i>');
   }
