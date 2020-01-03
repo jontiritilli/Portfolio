a8a0059376bd6bd5f0eff27627c393ccc33d0ff0
Jon Tiritilli <jonathantiritilli@gmail.com>
2020-01-03 14:54:35 -0800

    restyle buttons into neumorphism style

--- client/js/init.js
+++ client/js/init.js
@@ -1,36 +1,57 @@
 (function($){
   $(function () {
-    document.body.addEventListener('scroll', () => {
+    document.addEventListener('scroll', () => {
       addTopBtn();
     });
     $('#top-btn').on('click', () => {
-      topFunction();
+      scrollToTop();
     });
     $('a').on('click', (event) => {
       let scrolltarget = $(event.target).parent().attr("href");
-      scrollAndHide(scrolltarget);
+      scrollToTarget(scrolltarget);
     });
+    addButtonListeners();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
+function addButtonListeners() {
+  $('.neumorphic').on({
+    'mousedown': function(){
+      let element = $(this);
+      if(!element.hasClass('pressed')){
+        element.addClass('pressed');
+      }
+    },
+    'mouseup': function(){
+      let element = $(this);
+      if(element.hasClass('pressed')){
+        element.removeClass('pressed');
+      }
+    }
+  })
+}
 function addTopBtn(){
   let showPosition = 82;
-  let scrollPos = $(document.body).scrollTop();
+  let scrollPos = $(document).scrollTop();
+  console.log(scrollPos)
   if (scrollPos > showPosition) {
     $('#top-btn').removeClass('scale-out');
   } else {
     $('#top-btn').addClass('scale-out');
   }
 }
-function topFunction() {
-  $('html, body').animate({
+function scrollToTop() {
+  $('body').animate({
     scrollTop: $(`body`).offset().top
   }, 750);
 }
-function scrollAndHide(target){
+function scrollToTarget(target){
   if(target){
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 750);
+    console.log('target', target.slice(1,target.length));
+    let element = document.getElementById(target.slice(1,target.length))
+    console.log('element', element)
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    // $('body').animate({
+    //   scrollTop: $(target).offset().top
+    // }, 750);
   }
 }
