9ff75667b607dd3a90e0f705ea2cea7c0e998c5c
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2020-02-08 06:51:42 -0800

    auto update footer date, clean up styling, fix scroll

--- client/js/init.js
+++ client/js/init.js
@@ -1,57 +1,63 @@
-(function($){
-  $(function () {
-    document.addEventListener('scroll', () => {
-      addTopBtn();
-    });
-    $('#top-btn').on('click', () => {
-      scrollToTop();
-    });
-    $('a').on('click', (event) => {
-      let scrolltarget = $(event.target).parent().attr("href");
-      scrollToTarget(scrolltarget);
-    });
-    addButtonListeners();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-function addButtonListeners() {
-  $('.neumorphic').on({
-    'mousedown': function(){
+$(document).ready(function() {
+  document.addEventListener("scroll", () => {
+    addTopBtn();
+  });
+  addClickHandlers();
+  setFooterDate();
+});
+
+function setFooterDate() {
+  var date = new Date();
+  var year = date.getFullYear();
+  $("#footer-year").text(year);
+}
+function addClickHandlers() {
+  $("#top-btn").on("click", () => {
+    scrollToTop();
+  });
+  $("a").on("click", event => {
+    let scrolltarget = $(event.currentTarget).attr("href");
+    scrollToTarget(scrolltarget);
+  });
+  $(".neumorphic").on({
+    mousedown: function() {
       let element = $(this);
-      if(!element.hasClass('pressed')){
-        element.addClass('pressed');
+      if (!element.hasClass("pressed")) {
+        element.addClass("pressed");
       }
     },
-    'mouseup': function(){
+    mouseup: function() {
       let element = $(this);
-      if(element.hasClass('pressed')){
-        element.removeClass('pressed');
+      if (element.hasClass("pressed")) {
+        element.removeClass("pressed");
       }
     }
-  })
+  });
 }
-function addTopBtn(){
+function addTopBtn() {
   let showPosition = 82;
   let scrollPos = $(document).scrollTop();
-  console.log(scrollPos)
   if (scrollPos > showPosition) {
-    $('#top-btn').removeClass('scale-out');
+    $("#top-btn").removeClass("scale-out");
   } else {
-    $('#top-btn').addClass('scale-out');
+    $("#top-btn").addClass("scale-out");
   }
 }
 function scrollToTop() {
-  $('body').animate({
-    scrollTop: $(`body`).offset().top
-  }, 750);
+  $("body").animate(
+    {
+      scrollTop: $("body").offset().top
+    },
+    750
+  );
 }
-function scrollToTarget(target){
-  if(target){
-    console.log('target', target.slice(1,target.length));
-    let element = document.getElementById(target.slice(1,target.length))
-    console.log('element', element)
-    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    // $('body').animate({
-    //   scrollTop: $(target).offset().top
-    // }, 750);
+function scrollToTarget(target) {
+  if (target) {
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top
+      },
+      750
+    );
   }
 }
